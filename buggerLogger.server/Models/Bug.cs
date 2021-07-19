bdd6134dd53beeea74e923076b0628d695a8be82
isabellesavannah <isabellet.208@gmail.com>
2021-07-19 10:30:19 -0600

    Finally got mysql table to work for bugs.

--- buggerLogger.server/Models/Bug.cs
+++ buggerLogger.server/Models/Bug.cs
@@ -6,12 +6,12 @@ namespace buggerLogger.Models
 {
   public class Bug
   {
+    public int Id {get; set;}
     public bool Closed { get; set; }
     [Required]
     public string Description { get; set; }
     [Required]
     public string Title { get; set; }
-    public DateTime ClosedDate { get; set; }
     [Required]
     public string CreatorId { get; set; }
     public Profile Creator { get; set; }
